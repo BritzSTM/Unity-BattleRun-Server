7a5d2f0f2efb317e5c5ac9fa645cb2cbb6f56984
BritzSTM <ha40672@gmail.com>
2021-10-04 03:52:15 +0900

    WIP

--- Entry.js
+++ Entry.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const bitset_1 = require("bitset");
-var tmp = async function () {
+var _testAddAchivements = function () {
     let bs = new bitset_1.default("0".repeat(1024));
     bs.set(511, 1);
     let formated = ("0".repeat(1024) + bs.toString()).slice(-1024);
-    console.log(formated);
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    var updateUserDataResult = server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            "_A1": formated
+        }
+    });
 };
-tmp();
 //# sourceMappingURL=Entry.js.map
