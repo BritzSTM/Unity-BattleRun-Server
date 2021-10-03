8cb71d5569701a65462957c9ea83de01dd71578c
BritzSTM <ha40672@gmail.com>
2021-10-04 03:48:22 +0900

    Move desc playfab scripts, add 도전과제 초기화 코드

--- Entry.js
+++ Entry.js
@@ -1,1 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const bitset_1 = require("bitset");
+var tmp = async function () {
+    let bs = new bitset_1.default("0".repeat(1024));
+    bs.set(511, 1);
+    let formated = ("0".repeat(1024) + bs.toString()).slice(-1024);
+    console.log(formated);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+};
+tmp();
 //# sourceMappingURL=Entry.js.map
--- Entry.ts
+++ Entry.ts
@@ -1,5 +1,16 @@
-var tmp = function () {
-    let bs = new BitSet;
-    bs.set(128, 1);
-    console.log(bs.toString(16));
+import BitSet from 'bitset'
+
+var _testAddAchivements = function () {
+
+    let bs = new BitSet("0".repeat(1024));
+    bs.set(511, 1);
+
+    let formated = ("0".repeat(1024) + bs.toString()).slice(-1024);
+
+    var updateUserDataResult = server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            "_A1": formated
+        }
+    });
 }
